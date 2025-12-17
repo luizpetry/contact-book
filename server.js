32fcaa5a257ca4d9b46d994945657b7fb0c329c0
frostyez <juniorpetry.k1@gmail.com>
2025-12-17 14:09:36 -0300

    Updating server.js & package.json scripts

--- server.js
+++ server.js
@@ -2,41 +2,43 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  })
+
+// --- CORREÇÃO AQUI ---
+// Removidas as opções depreciadas (useNewUrlParser, etc)
+// O Mongoose moderno conecta apenas com a string.
+mongoose.connect(process.env.CONNECTIONSTRING)
   .then(() => {
     app.emit('pronto');
   })
   .catch(e => console.log(e));
+
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const routes = require('./routes');
 const path = require('path');
-// const helmet = require('helmet'); // helmet começou a causar problemas no localhost por conta da falta de SSL
+// const helmet = require('helmet'); // Mantido comentado conforme seu pedido
 const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middleware');
 
-// app.use(helmet()); // helmet começou a causar problemas no localhost por conta da falta de SSL
+// app.use(helmet()); // Mantido comentado
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
-  secret: 'akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()',
+  secret: 'akasdfj0út23453456+54qt23qv qwf qwer qwer qewr asdasdasda a6()',
+  // Configuração correta do MongoStore (com mongoUrl)
   store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dias
     httpOnly: true
   }
 });
+
 app.use(sessionOptions);
 app.use(flash());
 
@@ -44,6 +46,7 @@ app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(csrf());
+
 // Nossos próprios middlewares
 app.use(middlewareGlobal);
 app.use(checkCsrfError);
